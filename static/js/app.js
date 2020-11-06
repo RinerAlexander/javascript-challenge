9b999f5b2a3a1dab4cc765ba1a2233af66b3c90c
Alex Riner <rineralexander@gmail.com>
2020-11-06 17:19:13 -0600

    finished first section

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
+
+button.on("click", function() {
+
+    var input = d3.select("#datetime").property("value");
+
+
+    var results = timeFilter(input);
+
+    tbody.text("");
+    
+    results.forEach(entry => {
+      
+        var row = tbody.append("tr");
+      
+        Object.entries(entry).forEach(([key, value]) => {
+
+          console.log(key, value);
+          var cell = row.append("td");
+          cell.text(value);
+
+        });
+      });
+  });
+
+function timeFilter(date){
+    return tableData.filter(entry => entry.datetime==date);
+};
